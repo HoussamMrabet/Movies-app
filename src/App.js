0f58a088d60bcc7c25db5f513b315bbbbbb7693b
Houssam Mrabet <dodslim100@gmail.com>
2023-02-14 14:41:28 +0100

    api adjust

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=d0c469b0";
+const API_URL = "https://www.omdbapi.com?apikey=d0c469b0";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
